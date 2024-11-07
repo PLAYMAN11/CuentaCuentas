0f1eec8f0b65c4507ca9dde0f8cf99af44fcea4e
andrikesquer <160580850+andrikesquer@users.noreply.github.com>
2024-11-07 12:41:53 -0600

    Crear identificador random
    Se crea una clase con un método para crear una cadena de extensión 10 con caracteres y números aleatorios.

---
+++ app/src/main/java/com/cuentacuentas/RandomStringGenerator.java
@@ -0,0 +1,18 @@
+package com.cuentacuentas;
+
+import java.security.SecureRandom;
+
+public class RandomStringGenerator {
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static final int LENGTH = 10;
+    private static final SecureRandom random = new SecureRandom();
+
+    public static String generateRandomString() {
+        StringBuilder sb = new StringBuilder(LENGTH);
+        for (int i = 0; i < LENGTH; i++) {
+            int randomIndex = random.nextInt(CHARACTERS.length());
+            sb.append(CHARACTERS.charAt(randomIndex));
+        }
+        return sb.toString();
+    }
+}
