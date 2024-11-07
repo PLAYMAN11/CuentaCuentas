535910812f6e9cb166e6011ac80c469411dd11f1
Emmanuel <EmmanuelGZHZ@hotmail.com>
2024-11-07 12:47:33 -0600

    Se crea la base de datos, se realiza la conexion y se prepara para la insercion de datos, aun no esta preparada para jalar datos.

--- app/src/main/java/com/cuentacuentas/MainActivity.java
+++ app/src/main/java/com/cuentacuentas/MainActivity.java
@@ -1,14 +1,25 @@
 package com.cuentacuentas;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,29 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+        Map<String, Object> USUARIO = new HashMap<>();
+        USUARIO.put("Nombre","VARIABLE_NOMBRE_AQUI");
+        USUARIO.put("Producto","VALOR_PRODUCTO_AQUI");
+        USUARIO.put("Valor","VALOR_PRODUCTO_AQUI");
+        
+        //VARIABLE ALEATORIO;
+        db.collection("*VARIABLE VALOR ALEATORIO AQUI*")
+                .add(USUARIO)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d("Firestore", "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("Firestore", "Error adding document", e);
+                    }
+                });
     }
 }
